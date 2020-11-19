ac7229966878f79e58eb38152a38d45bd61386f6
alan-figueroa-iglesias <72175280+alan-figueroa-iglesias@users.noreply.github.com>
2020-11-18 20:00:37 -0600

    Add files via upload

---
+++ 1p/ej/Sets/nombres.js
@@ -0,0 +1,14 @@
+console.log("Agregar 10 nombres e imprimir");
+
+const nombres = new Set(["Pepe", 
+                        "Lalo", 
+                        "Karla", 
+                        "Luis", 
+                        "Javier", 
+                        "María", 
+                        "Alex", 
+                        "Katia", 
+                        "Alejandra", 
+                        "Fernanda"]);
+
+console.log(nombres);
